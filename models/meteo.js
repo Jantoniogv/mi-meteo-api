5e48b9a8312c93dee1915b64df466dd9a6a10d25
Jose Antonio GV <jantoniogv85@gmail.com>
2022-09-15 19:59:26 +0200

    Correccion modelo DB

--- models/meteo.js
+++ models/meteo.js
@@ -3,10 +3,19 @@ const Schema = mongoose.Schema;
 
 const MeteoSchema = Schema({
   location: String,
+  date: {
+    type: date,
+    default: new date(),
+  },
   temp: Number,
   hum: Number,
   water: Number,
   pressure: Number,
+
+  avg_wind: Number,
+  min_wind: Number,
+  max_wind: Number,
+  dir_wind: String,
 });
 
 module.exports = mongoose.model("MeteoDates", MeteoSchema);
