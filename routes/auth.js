17b15edd989ccccba834055ddd12e54dc4dca5b5
Jose Antonio GV <jantoniogv85@gmail.com>
2022-09-23 20:25:25 +0200

    elminar mas archivos innecesarios

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const AuthControllers = require("../controllers/auth");
-
-const api = express.Router();
-
-api.post("/refresh-access-token", AuthControllers.refreshAccessToken);
-
-module.exports = api;
